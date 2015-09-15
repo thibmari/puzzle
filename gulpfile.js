12d394d8573b44dd6dfe94dfdab976d8a7141167
Thibault <thibaultmarin1@gmail.com>
2015-09-15 22:08:59 +0200

    initial commit puzzles

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+    gulp.src('sass/**/*.scss')
+        .pipe(sass.sync().on('error', sass.logError))
+        .pipe(gulp.dest('css'));
+});
---
+++ package.json.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Thibault on 10-9-2015.
+ */
